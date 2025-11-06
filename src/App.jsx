1a34c491aafa6e4fe090d0af975d7d5622cc10cf
Wilson Andres Carmona Barco <wilsonandres08@hotmail.com>
2025-11-06 16:12:00 +0000

    ajuste estilos generales y configuracion menu con react dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import './componentes/estilosGenerales.css'
-import Menu from './componentes/menu/menu'
+import Menu from './componentes/menu/menu.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    {/*menu, esto es un comentario en react*/}
+    <Router>
       <Menu />
-
+    {/*rutas para el funcionamiento del menu,!hay que modificarlo cuando esten loscomponentes front-end listos*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/verLiga" element={<Ligas />} />
+        <Route path="/jugadores" element={<Jugadores />} />
+        <Route path="/misEquipos" element={<Equipos />} />
+        <Route path="/ranking" element={<Ranking />} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/componentes/menu/menu.jsx
+++ src/componentes/menu/menu.jsx
@@ -6,20 +6,23 @@ const Menu = () => {
   return (
     <nav className="menu">
       <div className="menu__logo">
-        <h1>MiApp ⚽</h1>
+        <div className="logo_agus"></div>
+        <h1>Futbol de Fantasia</h1>
       </div>
 
       <ul className="menu__tabs">
-        <li><a href="#perfil">Perfil</a></li>
-        <li><a href="#equipo">Mi equipo</a></li>
-        <li><a href="#ligas">Ver ligas</a></li>
-        <li><a href="#ranking">Ranking</a></li>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/perfil">Perfil</Link></li>
+        <li><Link to="/verLiga">Ver ligas</Link></li>
+        <li><Link to="/jugadores">Jugadores</Link></li>
+        <li><Link to="/misEquipos">Mis Equipos</Link></li>
+        <li><Link to="/ranking">Ranking</Link></li>
       </ul>
 
-      <div className="menu__auth">
+      {/*<div className="menu__auth">
         <button className="btn btn--registro">Registro</button>
         <button className="btn btn--login">Login</button>
-      </div>
+      </div>*/}
     </nav>
   );
 };
