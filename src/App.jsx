5cdc1736d2e12b34e5dd6b6d23370ea0fb3d3d82
Juan-Caicedo27 <juan.caicedoc@uniagustiniana.edu.co>
2025-11-18 18:59:06 +0000

    conexion Juan caicedo - mis equipos

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ import PanelAdmin from "./componentes/administrador/PanelAdmin.jsx";
 import Jugadores from "./componentes/jugadores/Jugadores.jsx";
 import Hero from "./componentes/inicio/Hero";
 import SeleccionJugadores from "./componentes/seleccion-jugadores/seleccionJugadores.jsx";
-
+import Equipos from "./componentes/equipos/equipos.jsx";
 // ✅ Importar correctamente los archivos de Ligas
 import Ligas from "./componentes/ligas/Ligas.jsx";
 import "./componentes/ligas/Ligas.css";
+import { supabaseJuan } from '/src/lib/supabase.js';
 
 //Pruba conexion supabase base de datos Ligas
 import { supabase } from '/src/lib/supabase.js';
@@ -20,11 +21,16 @@ import { supabase } from '/src/lib/supabase.js';
 const test = await supabase.from("ligas").select("*");
 console.log(test);
 
+//
+
+
+
+
 //constates temporales para probar el menu (si tu componente tiene alguno de estos nombres solo comenta o elimina la constante de aqui abajo)
 //const Home = () => <h2>Home</h2>;
 const LigasTemp = () => <h2>Ligas</h2>; // 🔹 Renombrado para evitar conflicto
 const JugadoresTemp = () => <h2>Jugadores</h2>;
-const Equipos = () => <h2>Equipos</h2>;
+// const Equipos = () => <h2>Equipos</h2>; era temporal 
 const Ranking = () => <h2>Ranking</h2>;
 //const Admin =() => <h2>PanelAdmin</h2>
 
